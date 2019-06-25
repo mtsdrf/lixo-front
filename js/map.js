1f533953180754b31a2601315d73ed07c581d15b
= <mreisf.geral@gmail.com>
2019-06-25 09:25:12 -0300

    Layout updates

--- js/map.js
+++ js/map.js
@@ -13,26 +13,18 @@ var map = new ol.Map({
 
 function setMakers(elemento){
 
-    var element = document.createElement('div');
-    element.innerHTML = '<img src="https://cdn.mapmarker.io/api/v1/fa/stack?size=50&color=DC4C3F&icon=fa-microchip&hoffset=1"/>';
-    var marker = new ol.Overlay({
-        position: [-48.145180,-21.796910],
-        positioning: 'center-center',
-        element: element,
-        stopEvent: false
-    });
-    map.addOverlay(marker);
 
-    // var marker = new ol.Feature({
-    //     geometry: new ol.geom.Point(
-    //         ol.proj.fromLonLat([-48.145180,-21.796910])
-    //     ),
-    //   });
-    // var vectorSource = new ol.source.Vector({
-    //     features: [marker]
-    // });
-    // var markerVectorLayer = new ol.layer.Vector({
-    //     source: vectorSource,
-    // });
-    // map.addLayer(markerVectorLayer);
+
+    var marker = new ol.Feature({
+        geometry: new ol.geom.Point(
+            ol.proj.fromLonLat([-48.145180,-21.796910])
+        ),
+      });
+    var vectorSource = new ol.source.Vector({
+        features: [marker]
+    });
+    var markerVectorLayer = new ol.layer.Vector({
+        source: vectorSource,
+    });
+    map.addLayer(markerVectorLayer);
 }
